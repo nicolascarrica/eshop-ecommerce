17849cef83c1807f378beff6584a2102aeed5de2
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-01-04 20:45:17 -0300

    Create Footer Component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from './Footer.module.scss'
 
+const date = new Date()
+const year = date.getFullYear()
 const Footer = () => {
   return (
-    <div>
-      
+    <div className={styles.footer}>
+      &copy; {year} All Rights Reserved
     </div>
   )
 }
