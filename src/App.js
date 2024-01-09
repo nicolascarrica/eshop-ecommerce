6b0b8ea9b9eacc298f740398859a23ec1a1a12a2
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-01-09 20:55:57 -0300

    Add auth routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { Home, Contact } from "./pages/"
+import { Home, Contact, Login, Register, Reset } from "./pages/"
 import { Header, Footer } from "./components"
 
 
 
 function App() {
   return (
-    <>
       <BrowserRouter>
       <Header />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/contact" element={<Contact />}/>
-
-
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="/reset" element={<Reset />}/>
         </Routes>
       <Footer />
       </BrowserRouter>
-    </> 
   );
 }
 
