b68fa0524057736b95f8d91d08e3070d27c8e4a2
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-01-10 16:30:07 -0300

    Delete unnecessary import

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -3,7 +3,7 @@ import Card from '../../components/card/Card'
 import styles from "./auth.module.scss"
 import registerImg from "../../assets/register.png"
 import { Link } from 'react-router-dom'
-import { FaGoogle } from 'react-icons/fa'
+
 
 const Register = () => {
   return (
