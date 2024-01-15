e2a54c7fdce2cdce430959034a6310ac767056df
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-01-15 15:14:13 -0300

    Update: Add reset password logic

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../../components/card/Card'
 import styles from "./auth.module.scss"
 import resetImg from "../../assets/forgot.png"
 import { Link } from 'react-router-dom'
+import { auth } from '../../firebase/config'
+import { sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify'
+import Loader from '../../components/loader/Loader'
 
 const Reset = () => {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetPassword = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    sendPasswordResetEmail(auth, email)
+    .then(() => {
+      setIsLoading(false);
+      toast.success("Password reset link sent to your email");
+    })
+   .catch((error) => {
+      setIsLoading(false);
+      toast.error(error.message)
+    });
+  }
+
+
   return (
+    <>
+    {isLoading && <Loader/>}
     <section className={`container ${styles.auth}`}> 
 
         <div className={styles.img}> 
@@ -14,9 +38,15 @@ const Reset = () => {
         <Card>
           <div className={styles.form}>
               <h2>Reset Password</h2>
-              <form>
-                <input type="text" placeholder='Email' required/>
-                <button className='--btn --btn-primary --btn-block'>Reset Password</button>
+              <form onSubmit={resetPassword}>
+                <input
+                 type="text" 
+                 placeholder='Email' 
+                 required
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}
+                />
+                <button type ='submit' className='--btn --btn-primary --btn-block'>Reset Password</button>
                 <div className={styles.links}>
                   <p><Link to="/login">- Login</Link></p>
                   <p><Link to="/register">- Register</Link></p>
@@ -25,6 +55,7 @@ const Reset = () => {
           </div>
         </Card>
     </section>
+    </>
   )
 }
 
