03a321e0ee4d48e2797d37e1bfab93c38a423a9d
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-01-15 14:12:59 -0300

    Update: delete ToastContainer Component

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -5,7 +5,7 @@ import styles from "./auth.module.scss"
 import registerImg from "../../assets/register.png"
 import { Link, useNavigate } from 'react-router-dom'
 
-import { ToastContainer, toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { createUserWithEmailAndPassword } from "firebase/auth";
@@ -44,7 +44,6 @@ const Register = () => {
 
   return (
     <>
-    <ToastContainer /> 
     {isLoading && <Loader />}
     <section className={`container ${styles.auth}`}> 
         <Card>
