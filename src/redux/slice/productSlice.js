6fdaab10c4a80ceb15d54aa0833e2ee46bf31c7b
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-03 19:29:06 -0300

    Create product slice

---
+++ src/redux/slice/productSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { storeData } from "../../assets/carriData";
+
+const productSlice = createSlice({
+    name: "products",
+    initialState: {
+        filteredProducts: JSON.parse(localStorage.getItem("filteredData")) || storeData,
+        singleProduct: JSON.parse(localStorage.getItem("singleProduct")) || storeData,
+    },
+    reducers: {
+        filterProducts: (state, action) => {
+            try{
+                const filter = storeData.filter((product) => product.type === action.payload);
+                state.filteredProducts = filter;
+                state.error = false;
+                const saveState = JSON.stringify(filter)
+                sessionStorage.setItem("filteredData", saveState);
+            }
+            catch (error) {
+                state.error = true
+            }
+        },
+    }
+})
+
+export const {filterProducts} = productSlice.actions;
+export default productSlice.reducer
