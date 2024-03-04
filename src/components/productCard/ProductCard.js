2435b9c51b27f089113ed56616dcee089d92fff7
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-04 16:05:35 -0300

    add Link element in product card

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './ProductCard.module.scss'
+import { useDispatch } from 'react-redux';
+import { singleProduct } from '../../redux/slice/productSlice';
+import { Link, useParams} from 'react-router-dom';
 
 const ProductCard = ({ id, name, img, colors, price, size }) => {
+const dispatch = useDispatch();
+const { type } = useParams();
+
   return (
-    <div className={style['product-card']}>
-      <img src={img} alt={name} className={style['product-image']} />
-      <div className={style['product-details']}>
-        <h3 className={style['product-name']}>{name}</h3>
-        <p className={style['product-price']}>${price}</p>
-        <ul className={style['product-colors']}>
-          {colors?.map((color, index) => (
-            <li key={index} style={{ backgroundColor: color }} className={style['color-option']}></li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <Link to={`/filteredProducts/${type}/${id}`}>
+        <div className={style['product-card']} onClick={() => dispatch(singleProduct(id))}>
+          <img src={img} alt={name} className={style['product-image']} />
+          <div className={style['product-details']}>
+            <h3 className={style['product-name']}>{name}</h3>
+            <p className={style['product-price']}>${price}</p>
+            <ul className={style['product-colors']}>
+              {colors?.map((color, index) => (
+                <li key={index} style={{ backgroundColor: color }} className={style['color-option']}></li>
+              ))}
+            </ul>
+          </div>
+        </div>
+    </Link>
   );
 };
 
