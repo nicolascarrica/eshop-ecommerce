9cacc62b57fbcc2e0c51f06cc2caaf4b686948ce
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-04 12:19:44 -0300

    Add product reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slice/authSlice';
+import productReducer from './slice/productSlice';
 
 const rootReducer = combineReducers({
-    auth: authReducer, 
+    auth: authReducer,
+    products: productReducer
 });
 
 const store = configureStore({
