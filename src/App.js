2a53650cffcd2e7b318ba1b19e9fb0b39794a868
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-04 12:20:54 -0300

    Update: add product page Route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import { Home, Contact, Login, Register, Reset } from "./pages/"
 import { Header, Footer } from "./components"
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import FilteredProducts from "./pages/filteredProducts/FilteredProducts";
 
 
 
@@ -19,6 +18,7 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="/reset" element={<Reset />}/>
+          <Route path="/filteredProducts/:type" element={<FilteredProducts />}/>
         </Routes>
       <Footer />
       </BrowserRouter>
