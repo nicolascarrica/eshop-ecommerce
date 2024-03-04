746cd7ec87df755f11c5fe2f197bf90025b24c6f
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-04 12:19:29 -0300

    Add navigate Buttons Elements

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Slider from '../../components/slider/Slider'
+import NavigateButtons from '../../components/navigateButtons/NavigateButtons'
 
 const Home = () => {
   return (
     <div>
-      
+      <Slider />
+      <NavigateButtons />
     </div>
   )
 }
