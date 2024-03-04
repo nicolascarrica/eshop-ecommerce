8084fccb2ae8758e3fd64bd1b7d520a8d153d6ea
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-04 16:04:30 -0300

    Create AddtoCartButton Element

---
+++ src/components/addToCartButton/AddToCartButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './AddToCartButton.module.scss'
+
+const AddToCartButton = () => {
+  return (
+    <div>
+      <button className={styles.addToCartButton}> Add To cart</button>
+    </div>
+  )
+}
+
+export default AddToCartButton
