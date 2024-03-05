d0aeaa0d2ef5f30f25896fc1f60a71817ed84962
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-05 19:58:05 -0300

    Refactorize code

--- src/pages/Products/SingleProduct.js
+++ src/pages/Products/SingleProduct.js
@@ -5,25 +5,19 @@ import styles from './SingleProduct.module.scss';
 import NavigateButtons from '../../components/navigateButtons/NavigateButtons';
 import AddToCartButton from '../../components/addToCartButton/AddToCartButton';
 
+
+
 const SingleProduct = () => {
-  const product = useSelector((state) => state.products.singleProduct);
-  const productColor = product[0].color[0];
-  const productSize = product[0].size ? product[0].size[0] : "";
   const { id } = useParams();
-  const [color, setColor] = useState(productColor);
-  const [size, setSize] = useState(productSize);
+  const products = useSelector((state) => state.products.singleProduct);
+  const product = products.find((product) => product.id === id);
 
-  const [quantity, setQuantity] = useState(1);
+  const [selectedColor, setColor] = useState(product.color[0]);
+  const [selectedSize, setSize] = useState(product.size ? product.size[0] : '');
 
-  const handleIncrement = () => {
-    setQuantity(quantity + 1);
-  };
-
-  const handleDecrement = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  };
+  if (!product) {
+    return <div>Product not found</div>;
+  }
 
   return (
     <>
@@ -31,78 +25,61 @@ const SingleProduct = () => {
     <NavigateButtons/>
     </div>
     <div className={styles.product}>
-      {product
-        .filter((product)=> product.id === id)
-        .map((item, index) => {
-          return (
-            <div 
-              key={index}
+      <div>
+        <div>
+          <img 
+            src={product.img} alt={product.name} 
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.details}>
+          <h2 className={styles.name}>{product.name}</h2>
+          <p className={styles.price}> ${product.price}</p>
+          <div className={styles.selectors}>
+          <label htmlFor='color' className={styles.label}>Pick a Color</label>
+            <select 
+              className={styles.colorSelect}
+              id='color'
+              name='color'
+              value={selectedColor}
+              onChange={(e) => setColor(e.target.value)}
             >
-              <div>
-                <img 
-                  src={item.img} alt={item.name} 
-                  className={styles.image}
-                />
-              </div>
-              <div className={styles.details}>
-                <h2 className={styles.name}>{item.name}</h2>
-                <p className={styles.price}> ${item.price}</p>
-                <div className={styles.selectors}>
-                <label className={styles.label}>Pick a Color</label>
-                  <select 
-                    className={styles.colorSelect}
-                    id='color'
-                    name='color'
+              {product.color.map((color, index) => {
+                return (
+                  <option
+                    key={index}
                     value={color}
-                    onChange={(e) => setColor(e.target.value)}
                   >
-                    {item.color.map((color, index) => {
-                      return (
-                        <option
-                          key={index}
-                          value={color}
-                        >
-                          {color.toUpperCase()}
-                        </option>
-                      )
-                    })}
-                  </select>
-                  <label className={styles.label}>Pick a size</label>
-                  <select 
-                    className={styles.sizeSelect}
-                    id='size'
-                    name='size'
+                    {color.toUpperCase()}
+                  </option>
+                )
+              })}
+            </select>
+            <label htmlFor='size' className={styles.label}>Pick a size</label>
+            <select 
+              className={styles.sizeSelect}
+              id='size'
+              name='size'
+              value={selectedSize}
+              onChange={(e) => setSize(e.target.value)}
+            >
+              {product.size.map((size, index) => {
+                return (
+                  <option
+                    key={index}
                     value={size}
-                    onChange={(e) => setSize(e.target.value)}
                   >
-                    {item.size.map((size, index) => {
-                      return (
-                        <option
-                          key={index}
-                          value={size}
-                        >
-                          {size}
-                        </option>
-                      )
-                    })}
-                  </select>
-                </div> 
-              </div>
-              <div className={styles.quantity}>
-                <label htmlFor="quantity" className={styles.label}>Quantity:</label>
-                <div className={styles.quantityControls}>
-                  <button className={styles.quantityButton} onClick={handleDecrement}>-</button>
-                  <input type="text" id="quantity" className={styles.quantityInput} value={quantity} readOnly />
-                  <button className={styles.quantityButton} onClick={handleIncrement}>+</button>
-                </div>
-              </div> 
-              <div className={styles.product}>
-                <AddToCartButton />
-              </div>
-            </div>
-            
-          )
-        })}
+                    {size}
+                  </option>
+                )
+              })}
+            </select>
+          </div> 
+        </div>
+        <div className={styles.product}>
+        <AddToCartButton product={product} selectedSize={selectedSize} selectedColor={selectedColor} />
+        </div>
+      </div>
     </div>
     </>
   )
