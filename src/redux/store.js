a287be5619cf7b83eea5c53a70f41f1100b6263e
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-05 20:01:42 -0300

    Update: Add Cart Reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './slice/authSlice';
 import productReducer from './slice/productSlice';
+import cartReducer from './slice/cartSlice';
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    products: productReducer
+    products: productReducer,
+    cart: cartReducer,
 });
 
 const store = configureStore({
