c9c3ae92795a23687dbbcabe01e7fab31a488e21
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-13 13:35:56 -0300

    Update: Add condition for when the list of products does not exist

--- src/pages/Products/FilteredProducts.js
+++ src/pages/Products/FilteredProducts.js
@@ -8,33 +8,37 @@ import ProductCard from '../../components/productCard/ProductCard';
 const FilteredProducts = () => {
   const products = useSelector(state => state.products.filteredProducts);
   const { type } = useParams();
-
   return (
-
     <div className={styles['product-container']}>
       <NavigateButtons /> 
       <div>
         <h1 className={styles.title}>{type}</h1>
       </div>
       <div className={styles.grid}>
-        {products
-          .filter((product) => product.type === type)
-          .map((product, idex) => (
-            <div key={idex}>
-            <ProductCard
-              id={product.id}
-              name={product.name}
-              img={product.img}
-              colors={product.color}
-              size={product.size}
-              price={product.price}
-            />
-            </div>
-          ))}
+        {products.length > 0 ? (
+          products
+            .filter(product => product.type === type)
+            .map((product, index) => (
+              <div key={index}>
+                <ProductCard
+                  id={product.id}
+                  name={product.name}
+                  img={product.img}
+                  colors={product.color}
+                  size={product.size}
+                  price={product.price}
+                />
+              </div>
+            ))
+        ) : null}
       </div>
+      {products.length === 0 && (
+        <div className={styles.noProductsMessage}>
+         <p>No products yet</p> 
+        </div>
+      )}
     </div>
-
-  )
+  );
 
 }
 
