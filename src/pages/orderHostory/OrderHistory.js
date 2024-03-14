cdc017a9de37ec236b6557772b759ddd17deb42b
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-14 14:17:35 -0300

    Create order history element

---
+++ src/pages/orderHostory/OrderHistory.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './OrderHistory.module.scss'
+
+const OrderHistory = () => {
+  return (
+    <div className={styles.container}>
+      <h2>
+        Page under construction
+      </h2>
+    </div>
+  )
+}
+
+export default OrderHistory
