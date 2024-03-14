fbdf3e7c763d6ecd8f2bc442e3dd5516a4f9777e
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2024-03-14 14:18:05 -0300

    Update: Add random product component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from '../../components/slider/Slider'
 import NavigateButtons from '../../components/navigateButtons/NavigateButtons'
+import RandomProducts from '../../components/randomProducts/RandomProducts'
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <NavigateButtons />
+      <RandomProducts count={4} />
     </div>
   )
 }
